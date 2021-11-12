4a0c1b738fedaa41ccec495a9b62381018a08fee
israelalmeida03 <israelalmeida31@gmail.com>
2021-11-12 01:06:16 -0300

    salvando

--- js/script.js
+++ js/script.js
@@ -18,14 +18,16 @@ botao.addEventListener('click', inserirLista);
 
 function inserirLista(event) {
   event.preventDefault();
+  
+  let img = document.createElement('img');
+  img.setAttribute('src', imagem.value);
+  document.querySelector('#img').appendChild(img);
 
   document.querySelector('p').innerText = `
   Marca: ${document.querySelector('#marca').value}
   Modelo: ${document.querySelector('#modelo').value}
   Ano: ${document.querySelector('#ano').value}`;
 
-  let img = document.createElement('img');
-  img.setAttribute('src', imagem.value);
-  document.querySelector('div').appendChild(img);
+ 
 
 }
